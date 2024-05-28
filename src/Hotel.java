4918e37bfb66597573c5ee31367a15eed4588f79
GuamanFrancis <francisguaman909@gmail.com>
2024-05-28 00:24:41 -0500

    segundo commit con sus respectivos codigos

--- src/Hotel.java
+++ src/Hotel.java
@@ -1,2 +1,84 @@
+// crear una clase llamada hotel
 public class Hotel {
+    String Nombre;
+    String Direccion;
+    int numerodepisos;
+    double preciohabitacion;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        Direccion = direccion;
+    }
+
+    public int getNumerodepisos() {
+        return numerodepisos;
+    }
+
+    public void setNumerodepisos(int numerodepisos) {
+        this.numerodepisos = numerodepisos;
+    }
+
+    public double getPreciohabitacion() {
+        return preciohabitacion;
+    }
+
+    public void setPreciohabitacion(double preciohabitacion) {
+        this.preciohabitacion = preciohabitacion;
+    }
+
+    public Hotel(String Nombre, String Direccion, int numerodepisos, double preciohabitacion){
+        this.Nombre = Nombre;
+        this.Direccion = Direccion;
+        this.numerodepisos = numerodepisos;
+        this.preciohabitacion = preciohabitacion;
+
+    }
+
+
+    public void Modificar(String nuevonombre,String nuevaDir, int nuevoNpisos,double nuevoprecio){
+
+        System.out.println(" Datos anteriores \n");
+        imprimir();
+        System.out.println(" ---------------------------------------\n");
+        this.Nombre = nuevonombre;
+        this.Direccion = nuevaDir;
+        this.numerodepisos = nuevoNpisos;
+        this.preciohabitacion = nuevoprecio;
+        System.out.println(" Datos modificados exitosamente \n");
+        System.out.println(" Datos nuevos \n");
+
+    }
+
+    public void imprimir(){
+
+        System.out.println("Datos. \n");
+        System.out.println("Nombre: "+Nombre+"\n");
+        System.out.println("Direccion: "+Direccion+"\n");
+        System.out.println("Numero de pisos: "+numerodepisos+"\n");
+        System.out.println(" Precio de habitacion: "+preciohabitacion+"\n");
+        System.out.println("Gracias por usar el sistema \n");
+
+    }
+
+    public void Modificarvaloresnull(){
+
+
+
+
+    }
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,40 @@
 
+//Utilizando los principios de programación orientada a objetos, implementar un sistema que me permita registrar hoteles de una ciudad.
+
+//
 public class Main {
     public static void main(String[] args) {
+
+        Hotel hotel1 = new Hotel("Hotel Quito","AV 123",15,200);
+        Hotel hotel2 = new Hotel("Hotel Colon ","AV 6 de dicicembre",20,100);
+        Hotel hotel3 = new Hotel("Hotel Rocafuerte","AV 19 de agosto",10,150);
+        Hotel hotel4 = new Hotel("Hotel Virtud","AV Gnrl Gutierrez",25,250);
+        Hotel hotel5 = new Hotel("Hotel Sanluis","AV Inca",10,50);
+        Hotel hotel6 = new Hotel("","",0,0);
+        Hotel hotel7 = new Hotel("","",0,0);
+
+        hotel6.imprimir();
+
+        hotel6.setNombre("Hotel Guayaquil");
+        hotel6.setDireccion("AV Simon B");
+        hotel6.setNumerodepisos(10);
+        hotel6.setPreciohabitacion(150.00);
+        hotel7.setNombre("Hotel California");
+        hotel7.setDireccion("AV caracas");
+        hotel7.setNumerodepisos(20);
+        hotel7.setPreciohabitacion(155);
+        hotel6.imprimir();        hotel3.Modificar("sdfdsf","sdfsdfsd",47564,45646545);
+        hotel3.imprimir();
+
+
+
+
+
+
+
+
+
+
 
 
 
